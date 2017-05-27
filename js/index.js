b15ff7d41b5a8779fc7aefb8823efb2e609c1dea
Roger Garcia <roger@quero.education>
2017-05-27 11:26:30 -0300

    Started true

--- js/index.js
+++ js/index.js
@@ -161,8 +161,8 @@ function itsTime() {
 
 
 // CHANGE HERE
-var eventStarted  = true;
-var dateBegin = new Date(2017, 4, 26, 21,46,00); // WTF, o 4 é o mes, e quer dizer Maio .-.
+var eventStarted  = false;
+var dateBegin = new Date(2017, 4, 27, 21,46,00); // WTF, o 4 é o mes, e quer dizer Maio .-.
 
 var dateEnd   = new Date(dateBegin.getTime() + 24*60*60*1000 + 15*1000);
 
